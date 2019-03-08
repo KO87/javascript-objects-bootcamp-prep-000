4a3b538963801191ae8e1c901a9d4c39aa0a721a
Kim Oseland <koseland87@gmail.com>
2019-03-08 16:20:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { 'song':'title'};
 
 var updatePlaylist = function(playlist, artistName, songTitle) {
-  return Object.assign({}, Name, Title);
+  return Object.assign({}, artistName, songTitle);
 };
